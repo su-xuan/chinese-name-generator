f3f9323850d44e9ffcc47d235594e49b28e0deb9
Su Xuan <48014272+su-xuan@users.noreply.github.com>
2020-07-22 16:35:45 +0200

    phonetic new img height

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
-import './index.css';
-
-
+require('bootstrap/dist/css/bootstrap.min.css');
+require('bootstrap/dist/js/bootstrap.bundle.min');
+require('./index.css');
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
